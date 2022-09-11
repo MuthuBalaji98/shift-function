073dc3a6cc88d046eda592aa6b71d3b2435d7db2
MuthuBalaji98 <muthubalaji1998@gmail.com>
2022-09-11 21:15:10 +0530

    shift

---
+++ main.js
@@ -0,0 +1,7 @@
+let subject=["Tamil","English","Maths","Science","Social"];
+console.log("original array :",subject);
+console.log("length of the array is "+subject.length);
+console.log("method : array.shift()");
+subject.shift();
+console.log("change in array : ", subject);
+console.log("return: length of array after shift() is "+subject.length);
